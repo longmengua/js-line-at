2262f7c4f062d836b8c9dcd65a0a91e3b4bb95a3
Waltor <longmengua@gmail.com>
2024-02-11 00:15:42 +0800

    update

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 import { LineAtService } from './service/lineAtService';
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
 // Middleware to parse JSON in the request body
 app.use(bodyParser.json());
@@ -17,59 +17,123 @@ app.get('/', async (req: Request, res: Response) => {
 
 interface LineEvent {
   type: string;
-  // Define other properties based on the event types
-  message?: any;
-  follow?: any;
   unfollow?: any;
-  // Define properties for other event types
+  postback?: {
+    data?: string;
+    params?: {
+      datetime?: string; // this will cause problem while in different time zone
+    }
+  }; 
+  beacon?: {
+    hwid?: string;
+    type?: string;
+  };  
+  message?: {
+    type?: string;
+    id?: string;
+    quoteToken?: string;
+    stickerId?: string;
+    packageId?: string;
+    stickerResourceType?: string;
+    keywords?: string[];
+    text?: string;
+    title?: string;
+    address?: string;
+    latitude?: number;
+    longitude?: number;
+    fileName?: string;
+    fileSize?: number;
+    duration?: number;
+    contentProvider?: {
+      type?: string;
+      originalContentUrl?: string;
+      previewImageUrl?: string;
+    };
+    imageSet?: {
+      id?: string;
+      index?: number;
+      totla?: number;
+    };
+    emojis?: Array<{
+      index?: number
+      length?: number
+      productId?: string
+      emojiId?: string
+    }>;
+    mention?: {
+      mentionees?: Array<{
+        index: number
+        length: number
+        type: string
+        userId?: string
+      }>
+    }
+  };
+  replyToken?: string;
+  mode?: string;
+  timestamp?: number;
+  source?: {
+    type?: string;
+    userId?: string;
+    groupId?: string;
+  };
+  webhookEventId?: string;
+  deliveryContext?: {
+    isRedelivery?: string;
+  };
+  follow?: {
+    isUnblocked?: boolean;
+  };
+  unsend?: {
+    messageId?: string;
+  }
 }
 
 app.post('/webhook', (req, res) => {
   const events = req.body.events;
+
   events.forEach((event: LineEvent) => {
     switch (event.type) {
       case 'message':
         // Handle message event
-        console.log('Message received:', event.message);
+        res.sendStatus(200).json({ code: '200001', data: event?.message, error: null, message: 'Message received' });
         break;
       case 'follow':
         // Handle follow event
-        console.log('User added you as a friend');
+        res.sendStatus(200).json({ code: '200002', data: { userId: event?.source?.userId }, error: null, message: 'User added you as a friend' });
         break;
       case 'unfollow':
         // Handle unfollow event
-        console.log('User removed you as a friend');
+        res.sendStatus(200).json({ code: '200003', data: { userId: event?.source?.userId }, error: null, message: 'User removed you as a friend' });
         break;
       case 'join':
         // Handle join event
-        console.log('User joined a group or added you as a friend in a multi-person chat');
+        res.sendStatus(200).json({ code: '200004', data: { userId: event?.source?.userId }, error: null, message: 'User joined a group or added you as a friend in a multi-person chat' });
         break;
       case 'leave':
         // Handle leave event
-        console.log('User left a group');
+        res.sendStatus(200).json({ code: '200005', data: { userId: event?.source?.userId }, error: null, message: 'User left a group' });
         break;
       case 'memberJoined':
         // Handle memberJoined event
-        console.log('User joined a group');
+        res.sendStatus(200).json({ code: '200006', data: { userId: event?.source?.userId }, error: null, message: 'User joined a group' });
         break;
       case 'memberLeft':
         // Handle memberLeft event
-        console.log('User left a group');
+        res.sendStatus(200).json({ code: '200006', data: { userId: event?.source?.userId }, error: null, message: 'User left a group' });
         break;
       case 'postback':
         // Handle postback event
-        console.log('Postback action received:', event.postback);
+        res.sendStatus(200).json({ code: '200007', data: { userId: event?.source?.userId }, error: null, message: 'Postback action received' });
         break;
       case 'beacon':
         // Handle beacon event
-        console.log('User entered the range of a beacon:', event.beacon);
+        res.sendStatus(200).json({ code: '200008', data: { userId: event?.source?.userId }, error: null, message: 'User entered the range of a beacon' });
         break;
       default:
-        console.log('Unknown event type:', event.type);
+        res.sendStatus(200).json({ code: '200999', data: { userId: event?.source?.userId }, error: null, message: `Unhandled event type: ${event.type}` });
     }
   });
-
-  res.sendStatus(200).json({ code: '200', data: 'ok', error: null, message: null });
 });
 
 app.get('/demo', async (req: Request, res: Response) => {
